d3dfaae3b6167092e318425551c51e93022b725c
Levhcwm21. <alexags1983@gmail.com>
2026-05-10 19:16:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -95,7 +95,7 @@ async def start(client, message):
                         InlineKeyboardButton('ᴀʙᴏᴜᴛ 📜', callback_data='about'),
                         InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info")
                    ],[
-                        InlineKeyboardButton('🌸 ᴀɴɪᴍᴇ ɢᴜɪᴅᴇ 🌸', user_id=int(OWNER))
+                        InlineKeyboardButton('• ADMIN •', user_id=int(OWNER))
                     ]]
             reply_markup = InlineKeyboardMarkup(buttons)
             m=await message.reply_sticker("CAACAgUAAxkBAAI47WjK1V24t_kyUL-ywJQQdxtaWnaeAAIMFgACpzEZVdpZS0jMvfn5HgQ")
